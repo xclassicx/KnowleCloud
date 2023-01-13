44545663a701fd7e2dba3eee01594ea4bd1bf3cc
Dmitry <xclassic69+hithub@gmail.com>
2023-01-13 16:23:30 +0300

    hide filepath on about page

--- views/site/about.php
+++ views/site/about.php
@@ -5,5 +5,3 @@ use yii\helpers\Html;
 $this->title = 'О проекте';
 ?>
 <h1 class="mt-5"><?= Html::encode($this->title) ?></h1>
-<p>This is the About page. You may modify the following file to customize its content:</p>
-<code><?= __FILE__ ?></code>
